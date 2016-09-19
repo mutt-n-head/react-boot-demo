8147af32297bf2a08759f188f640a0cc76f92642
Mutt_n_head <lewis.isner@gmail.com>
2016-09-19 11:40:36 -0400

    Changed application

--- app.js
+++ app.js
@@ -1,123 +1,163 @@
-var fs = require('fs');
-var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var moment = require('moment');
+var db = require('./db.js');
+var bodyParser = require('body-parser')
 
-var instagdb = require('./db.js');
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
-
-app.set('port', (process.env.PORT || 3000));
-
-app.use('/static', express.static(path.join(__dirname, 'build')));
+// parse application/json
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static('build'));
+app.use(express.static('public'));
+
+app.post('/login', function(req, res) {
+    var user = req.body.userid;
+    var pass = req.body.pass;
+
+    legit = db.selectUser(user);
+
+    legit.then(
+        (data) => {
+            console.log(data);
+            if (data.password === pass) {
+                res.status(200).send('success');
+            } else {
+                res.status(404).send('failure');
+            }
+        }
+    );
 });
 
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/build/index.html');
 });
 
-app.get('/api/comments:imageid', function(req, res) {  
-    var imageId = req.params.imageid;
-    console.log("I'm getting comments from database for image: " + imageId);
-    instagdb.selectCommentsForImage(imageId).then(
-        data => {
-            res.json(JSON.parse(data));
+app.get('/user/:id', function(req, res) {
+    var p = db.selectUser(req.params.id);
 
-        }).catch(
-        err => {
-            //handle all errors
-            console.log(err);
-            response.status(500);
-            response.send();
-        });
-
-  // fs.readFile(COMMENTS_FILE, function(err, data) {
-  //   if (err) {
-  //     console.error(err);
-  //     process.exit(1);
-  //   }
-  //   res.json(JSON.parse(data));
-  // });
-});
-
-app.get('/comments', function(req, res) {
-
-  console.log("I'm getting all the comments from database");
-  instagdb.selectAllComments().then(
-        data => {
-            res.send(data);
-        }).catch(
-        err => {
-            console.log(err);
-            response.status(500);
-            response.send();
-        });
-});
-
-app.post('/comment', function(request, response) {
-  var userid = request.body.USERID;
-  var MESSAGE = request.body.MESSAGE;
-//   var timestamp = moment().format('YYYY-MM-DD H:mm:ss');
-  instagdb.insertComment(2, userid, MESSAGE)
-    .then(
-      () => {
-          response.send("success added");
-      }).catch(err => {
-              console.log(err);
-              response.status(500);
-              response.send(err);                
-      });
-
-  // fs.readFile(COMMENTS_FILE, function(err, data) {
-  //   if (err) {
-  //     console.error(err);
-  //     process.exit(1);
-  //   }
-  //   var comments = JSON.parse(data);
-
-  //   var newComment = {
-  //     id: Date.now(),
-  //     author: req.body.author,
-  //     text: req.body.text,
-  //   };
-  //   comments.push(newComment);
-  //   fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-  //     if (err) {
-  //       console.error(err);
-  //       process.exit(1);
-  //     }
-  //     res.json(comments);
-  //   });
-  // });
-});
-
-
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-  var p = instagdb.initDB();
     p.then(
-      val => {
-        instagdb.insertUser("1", "Connie", "pwd", "occasional user");
-        instagdb.insertImage('testimage1.jpg', '1');
-        instagdb.insertComment(1, '1', "first test comment");
-      }).catch(
-      err => {
-          //handle all errors
-          console.log(err);
-      });
+        (tweets) => {
+            res.status(200).send(tweets);
+        }
+    );
+});
 
+app.get('/images/:id', function(req, res) {
+    var p = db.selectImagesFor(req.params.id);
+
+    p.then(
+        (tweets) => {
+            res.status(200).send(tweets);
+        }
+    );
+});
+
+app.get('/followers/:id', function(req, res) {
+    var p = db.getFollowers(req.params.id);
+
+    p.then(
+        (followers) => {
+            res.status(200).send(followers);
+        }
+    );
+});
+
+app.get('/numfollowing/:id', (req, res) => {
+    var p = db.getNumThatFollowUser(req.params.id);
+
+    p.then(
+        (data) => {
+            res.status(200).send(data.toString());
+        },
+        (err) => {
+            res.status(404).send('failure');
+        }
+    );
+});
+
+app.get('/getpostcount/:id', (req, res) => {
+    var p = db.getPostCount(req.params.id);
+    p.then(
+        (data) => {
+            res.status(200).send(data.toString());
+        },
+        (err) => {
+            res.status(404).send('error');
+        }
+    );
+});
+
+app.post('/putimage', function(req, res) {
+    var p = db.insertImage(req.body.filename, req.body.userid);
+
+    p.then(
+        (tweets) => {
+            res.status(200).send('success');
+        },
+        (err) => {
+            res.status(404).send('fail');
+        }
+    );
+});
+
+app.post('/like', function(req, res) {
+
+});
+
+app.get('/feed/:id', function(req, res) {
+    var p = db.selectUserFeed(req.params.id);
+
+    p.then(
+        (tweets) => {
+            res.status(200).send(tweets);
+        }
+    );
+});
+
+app.get('/comments/:iid', (req, res) => {
+    var p = db.selectCommentsForImage(req.params.iid);
+
+    p.then(
+        (data) => {
+            res.status(200).send(data);
+        },
+        (err) => {
+            res.status(404).send('failed to retrive comments');
+        }
+    )
+});
+
+app.post('/comment', function(req, res) {
+    // Need image id, user id and comment string from post.
+    var p = db.insertCommentForImage(req.body.iid, req.body.userid, req.body.message);
+
+    p.then(
+        (data) => {
+            res.status(200).send("success");
+        },
+        (err) => {
+            res.status(404).send("failure");
+        }
+    )
+});
+
+app.post('/newuser', function(req, res) {
+    var p = db.insertUser(req.body.userid, req.body.username, req.body.password, req.body.profile);
+
+    p.then(
+        (data) => {
+            res.status(200).send('success');
+        },
+        (err) => {
+            res.status(404).send("failure");
+        }
+    )
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
